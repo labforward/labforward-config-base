d6b4a03a239431ea781e9622115e08a123a57fd6
Ignatius Reza <lyoneil.de.sire@gmail.com>
2021-02-21 09:31:37 +0100

    upgrade dependencies
    babel-eslint is now @babel/eslit-parser

--- prettier/index.js
+++ prettier/index.js
@@ -1,11 +1,3 @@
 module.exports = {
   singleQuote: true,
-  overrides: [
-    {
-      files: ['*.tsx'],
-      options: {
-        parser: 'babel-ts', // FIXME: somehow default detected "typescript" parser does not work with .tsx file
-      },
-    },
-  ],
 };
